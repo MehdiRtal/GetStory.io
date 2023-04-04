4df6f6c3521987ddcb6ebbe27a5f79f80c8334b7
Mehdi Rtal <mehdirtal7@pm.me>
2023-04-04 13:34:40 -0700

     

--- frontend/app.vue
+++ frontend/app.vue
@@ -7,11 +7,13 @@
 <script setup>
     useHead({
         titleTemplate: "%s - GetStory.io",
-    });
-
-    useServerSeoMeta({
-        ogTitle: "GetStory.io",
-        ogDescription:
-            "Download Instagram Stories quickly, easily and only in good quality. Seemed impossible? Now it is real.",
+        meta: [
+            {name: "title", content: "GetStory.io"},
+            {
+                name: "description",
+                content:
+                    "Download Instagram Stories quickly, easily and only in good quality. Seemed impossible? Now it is real.",
+            },
+        ],
     });
 </script>
